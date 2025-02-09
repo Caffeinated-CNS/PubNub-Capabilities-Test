300abf8303c87b269ad284ad52335d163bb153ae
Caffeinated-CNS <grant.meyers@outlook.com>
2025-02-08 22:57:33 -0700

    Initial commit and git ignore settings.
    Initial commit for future app placement and ignoring content not meant for public viewing.

---
+++ servers/PubNub_Loader/src/main/java/com/test/PubNub_Loader/App.java
@@ -0,0 +1,13 @@
+package com.test.PubNub_Loader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
