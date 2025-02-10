3e46865a0cde22b606618c91dc0e4f226c3624d7
Caffeinated-CNS <grant.meyers@outlook.com>
2025-02-09 19:19:43 -0700

    Example YAML export and PublisherSettings config classes
    Added an example YAML config file (with obvious testing values).
    Added updated PublisherSettings config classes for export that also works for loading exported files.

--- utils/YAMLConfigCreator/src/main/java/com/test/yaml_config_creator/load_target_classes/BasicSubscriberConfigSettings.java
+++ utils/YAMLConfigCreator/src/main/java/com/test/yaml_config_creator/load_target_classes/BasicSubscriberConfigSettings.java
@@ -1,28 +1,31 @@
 package com.test.yaml_config_creator.load_target_classes;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonSetter;
 
-import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.ToString;
 
 @ToString
-@AllArgsConstructor
 public class BasicSubscriberConfigSettings {
 	@NonNull
 	private String pubNubUser;
 	@NonNull
 	private String pubNubSubscribeKey;
 	@NonNull
-	private List<PubNubSubscription> pubNubSubscriptions;
+	private ArrayList<PubNubSubscription> pubNubSubscriptions = new ArrayList<>();
+	
+	
 
 	@JsonGetter
 	public String getPubNubUser() {
 		return pubNubUser;
 	}
 
+	@JsonSetter
 	public void setPubNubUser(String pubNubUser) {
 		this.pubNubUser = pubNubUser;
 	}
@@ -32,6 +35,7 @@ public class BasicSubscriberConfigSettings {
 		return pubNubSubscribeKey;
 	}
 
+	@JsonSetter
 	public void setPubNubSubscribeKey(String pubNubSubscribeKey) {
 		this.pubNubSubscribeKey = pubNubSubscribeKey;
 	}
@@ -41,8 +45,18 @@ public class BasicSubscriberConfigSettings {
 		return pubNubSubscriptions;
 	}
 
+	@JsonSetter
 	public void setPubNubSubscriptions(List<PubNubSubscription> pubNubSubscriptions) {
-		this.pubNubSubscriptions = pubNubSubscriptions;
+		this.pubNubSubscriptions.addAll(pubNubSubscriptions);
+	}
+	
+	public BasicSubscriberConfigSettings() {}
+
+	public BasicSubscriberConfigSettings(String pubNubUser, String pubNubSubscribeKey,
+			List<PubNubSubscription> pubNubSubscriptions) {
+		this.pubNubUser = pubNubUser;
+		this.pubNubSubscribeKey = pubNubSubscribeKey;
+		this.pubNubSubscriptions.addAll(pubNubSubscriptions);
 	}
 
 }
--- utils/YAMLConfigCreator/src/main/java/com/test/yaml_config_creator/load_target_classes/PubNubSubscription.java
+++ utils/YAMLConfigCreator/src/main/java/com/test/yaml_config_creator/load_target_classes/PubNubSubscription.java
@@ -3,7 +3,7 @@ package com.test.yaml_config_creator.load_target_classes;
 import com.fasterxml.jackson.annotation.JsonGetter;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
@@ -11,6 +11,7 @@ import lombok.ToString;
 @ToString
 @RequiredArgsConstructor(staticName = "of")
 @AllArgsConstructor
+@NoArgsConstructor
 public class PubNubSubscription {
 	@NonNull
 	private String channelName;
