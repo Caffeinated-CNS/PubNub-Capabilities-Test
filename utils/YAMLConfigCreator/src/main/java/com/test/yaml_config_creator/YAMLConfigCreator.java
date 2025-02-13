54d41a06d958a522fb52ebbaa37d197bbed3f6d6
Caffeinated-CNS <grant.meyers@outlook.com>
2025-02-12 21:40:13 -0700

    YAML Config Generator updated to support Access Token Generator app
    Updated YAML Config generator to create and export a config loadable into the Access Token Generator app.

--- utils/YAMLConfigCreator/src/main/java/com/test/yaml_config_creator/YAMLConfigCreator.java
+++ utils/YAMLConfigCreator/src/main/java/com/test/yaml_config_creator/YAMLConfigCreator.java
@@ -1,13 +1,12 @@
 package com.test.yaml_config_creator;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.util.Arrays;
 
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import com.test.yaml_config_creator.load_target_classes.BasicSubscriberConfigSettings;
-import com.test.yaml_config_creator.load_target_classes.PubNubSubscription;
-
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import com.test.yaml_config_creator.load_target_classes.token_gen.ChannelGrantConfig;
+import com.test.yaml_config_creator.load_target_classes.token_gen.TokenGenConfigSettings;
 
 public class YAMLConfigCreator {
 	private static String YAML_OUTPUT_PATH = "./configs_out/";
@@ -28,10 +27,10 @@ public class YAMLConfigCreator {
 		String outputFilePath = YAML_OUTPUT_PATH + sourceConfigPojoSimple + ".yaml";
 		File writeFile = new File(outputFilePath);
 		System.out.println("Target YAML filepath: " + writeFile.getAbsolutePath());
-		
+
 		YAMLMapper mapper = new YAMLMapper(new YAMLFactory());
 		mapper.writeValue(writeFile, configPOJOtoExport);
-		
+
 		System.out.println("Write Complete.  Exiting...");
 	}
 
@@ -42,20 +41,39 @@ public class YAMLConfigCreator {
 	 * 
 	 * @return generic reference to config POJO
 	 */
+//	private static Object configPojoPopulate() {
+//		ArrayList<PubNubSubscription> pubNubSubscriptions = new ArrayList<>();
+//
+//		pubNubSubscriptions.add(PubNubSubscription.of("Channel1"));
+//
+//		var a = PubNubSubscription.of("Channel2");
+//		a.setCursor("1");
+//		pubNubSubscriptions.add(a);
+//
+//		pubNubSubscriptions.add(PubNubSubscription.of("Channel3"));
+//
+//		BasicSubscriberConfigSettings configPOJOtoExport = new BasicSubscriberConfigSettings("basic_listener_username",
+//				"subscription_key_text", pubNubSubscriptions);
+//
+//		return configPOJOtoExport;
+//	}
+
 	private static Object configPojoPopulate() {
-		ArrayList<PubNubSubscription> pubNubSubscriptions = new ArrayList<>();
+		TokenGenConfigSettings tokenGenConfigSettings = new TokenGenConfigSettings();
 
-		pubNubSubscriptions.add(PubNubSubscription.of("Channel1"));
+		tokenGenConfigSettings.setPubNubPublishKey("pub-example-key");
+		tokenGenConfigSettings.setPubNubSubscribeKey("sub-example-key");
+		tokenGenConfigSettings.setPubNubUser("test_listener");
+		tokenGenConfigSettings.setSecret("sec-key-text");
+		tokenGenConfigSettings.setTtl(10);
+		
+		tokenGenConfigSettings.setChannelGrants(
+				Arrays.asList(
+						ChannelGrantConfig.of("Channel1", "", true, false),
+						ChannelGrantConfig.of("Channel2", "", true, false),
+						ChannelGrantConfig.of("Channel3", "", true, false)
+				));
 
-		var a = PubNubSubscription.of("Channel2");
-		a.setCursor("1");
-		pubNubSubscriptions.add(a);
-
-		pubNubSubscriptions.add(PubNubSubscription.of("Channel3"));
-
-		BasicSubscriberConfigSettings configPOJOtoExport = new BasicSubscriberConfigSettings("basic_listener_username",
-				"subscription_key_text", pubNubSubscriptions);
-
-		return configPOJOtoExport;
+		return tokenGenConfigSettings;
 	}
 }
---
+++ utils/YAMLConfigCreator/src/main/java/com/test/yaml_config_creator/load_target_classes/token_gen/ChannelGrantConfig.java
@@ -0,0 +1,46 @@
+package com.test.yaml_config_creator.load_target_classes.token_gen;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor(staticName = "of")
+public class ChannelGrantConfig {
+	private String channelName = null;
+	private String channelPattern = null;
+	private boolean readEnabled;
+	private boolean writeEnabled;
+
+	public String getChannelName() {
+		return channelName;
+	}
+
+	public void setChannelName(String channelName) {
+		this.channelName = channelName;
+	}
+
+	public String getChannelPattern() {
+		return channelPattern;
+	}
+
+	public void setChannelPattern(String channelPattern) {
+		this.channelPattern = channelPattern;
+	}
+
+	public boolean isReadEnabled() {
+		return readEnabled;
+	}
+
+	public void setReadEnabled(boolean readEnabled) {
+		this.readEnabled = readEnabled;
+	}
+
+	public boolean isWriteEnabled() {
+		return writeEnabled;
+	}
+
+	public void setWriteEnabled(boolean writeEnabled) {
+		this.writeEnabled = writeEnabled;
+	}
+
+}
